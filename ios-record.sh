5db9a083a41d4ed37f777f3f1e3d743b614253a6
Brian Haak <Haakman@users.noreply.github.com>
2019-10-02 13:12:47 -0400

    added usage comments

--- ios-record.sh
+++ ios-record.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# For creating a screen recording of an iOS simulator.
+# 1. Launch simlulator
+# 2. run script to start recording
+# 3. kill script with Ctrl+C
+# 4. file will be saved as recording.mp4 unless you pass in a filename as argument
+
 filename=$1
 
 if [ -z $filename ] ; then
